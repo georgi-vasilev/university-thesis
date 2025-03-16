93d48fa9ebe8c133166cf96cd63ac428cb48c5c9
Georgi Vasilev <35624206+georgi-vasilev@users.noreply.github.com>
2025-03-16 14:52:34 +0200

    feature: add domain configuration (#8)

---
+++ server/Domain/DomainConfiguration.cs
@@ -0,0 +1,35 @@
+﻿namespace Domain
+{
+    using Common;
+    using Event.Service;
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Reflection;
+
+
+    // This configuration is subject to change over time. 
+    // Adding this at the moment as something initial so that I don't forget to do this kind of
+    // Abstraction in the future for rest of the layers.
+    public static class DomainConfiguration
+    {
+        public static IServiceCollection AddDomain(this IServiceCollection services)
+            => services
+                .AddBuilders()
+                .AddDomainServices();
+
+        private static IServiceCollection AddBuilders(this IServiceCollection services)
+            => services
+                .Scan(scan => scan
+                    .FromAssemblies(Assembly.GetExecutingAssembly())
+                    .AddClasses(classes => classes.AssignableTo(typeof(IBuilder<>)))
+                    .AsImplementedInterfaces()
+                    .WithTransientLifetime());
+
+        private static IServiceCollection AddDomainServices(this IServiceCollection services)
+            => services
+                .Scan(scan => scan
+                    .FromAssemblies(Assembly.GetExecutingAssembly())
+                    .AddClasses(classes => classes.AssignableTo(typeof(IEventHostService)))
+                    .AsImplementedInterfaces()
+                    .WithTransientLifetime());
+    }
+}
