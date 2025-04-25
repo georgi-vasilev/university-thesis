5bd1323146fb2de48f6d821f7d17bd3b5c2f64dd
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-04-25 14:12:13 +0300

    feature: order purchase validator.

--- server/Application/Order/Commands/Purchase/OrderPurchaseCommandValidator.cs
+++ server/Application/Order/Commands/Purchase/OrderPurchaseCommandValidator.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Order.Commands.Purchase
+﻿namespace Application.Order.Commands.Purchase
 {
-    internal class OrderPurchaseCommandValidator
+    using FluentValidation;
+
+    public class OrderPurchaseCommandValidator : AbstractValidator<OrderPurchaseCommand>
     {
+        public OrderPurchaseCommandValidator()
+        {
+            RuleFor(cmd => cmd.BuyerId)
+                .NotEmpty()
+                    .WithMessage("Buyer ID must be provided.");
+
+            RuleFor(cmd => cmd.EventId)
+                .NotEmpty()
+                    .WithMessage("Event ID must be provided.");
+
+
+            RuleFor(cmd => cmd.PaymentAmount)
+                .NotNull()
+                    .WithMessage("PaymentAmount must be provided.")
+                .NotEmpty()
+                .Must(x => x.Amount > 0)
+                    .WithMessage("Amoumt must be more than 0.")
+                .Must(x => string.IsNullOrEmpty(x.Currency))
+                    .WithMessage("Currency must be provided.");
+        }
     }
 }
