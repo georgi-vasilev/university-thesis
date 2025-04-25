58500d906c205aea71ced85e0c969bef3eeaa78b
Georgi Vasilev <35624206+georgi-vasilev@users.noreply.github.com>
2025-04-25 15:53:09 +0300

    update: add logging/request behavior and register validators (#12)

--- server/Application/ApplicationConfiguration.cs
+++ server/Application/ApplicationConfiguration.cs
@@ -1,9 +1,11 @@
 ﻿namespace Application
 {
+    using Common.Behaviours;
+    using FluentValidation;
+    using MediatR;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using System.Reflection;
-
 
     // Again this configuration is subject to change
     // And mostly likely is in complete at the moment.
@@ -12,6 +14,11 @@
         public static IServiceCollection AddApplication(
             this IServiceCollection services,
             IConfiguration configuration)
-            => services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            => services
+                .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
+                .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>))
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+
     }
 }
