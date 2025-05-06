1cc4750870209ea9ec51a7f0aadfd1d4aeb3ceb2
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-05-06 09:36:19 +0300

    feature: add query validators

---
+++ server/Application/Events/Queries/GetEventsByHost/GetEventsByHostQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Events.Queries.GetEventsByHost
+{
+    using FluentValidation;
+
+    public class GetEventsByHostQueryValidator : AbstractValidator<GetEventsByHostQuery>
+    {
+        public GetEventsByHostQueryValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Host ID cannot be empty.");
+        }
+    }
+}
---
+++ server/Application/Events/Queries/GetEventsByVenue/GetEventsByVenueQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Events.Queries.GetEventsByVenue
+{
+    using FluentValidation;
+
+    public class GetEventsByVenueQueryValidator : AbstractValidator<GetEventsByVenueQuery>
+    {
+        public GetEventsByVenueQueryValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Venue ID cannot be empty.");
+        }
+    }
+}
