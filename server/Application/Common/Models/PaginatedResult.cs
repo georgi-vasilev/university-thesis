e85e645d4cc4144985b06a0cd3f96104f692871c
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-05-10 10:08:16 +0300

    feature: add paginated result object.

---
+++ server/Application/Common/Models/PaginatedResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Common.Models
+{
+    public record PaginatedResult<T>(
+     IEnumerable<T> Items,
+     int PageIndex,
+     int PageSize,
+     int TotalCount)
+    {
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+        public bool HasPreviousPage => PageIndex > 1;
+
+        public bool HasNextPage => PageIndex < TotalPages;
+    }
+}
