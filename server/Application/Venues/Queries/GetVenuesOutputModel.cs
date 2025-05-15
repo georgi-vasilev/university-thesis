f7096ebcff3dbde9c5361341c597f978853f9cba
Georgi Vasilev <35624206+georgi-vasilev@users.noreply.github.com>
2025-05-15 17:33:48 +0300

    add: venues controller, repository, service (#24)

---
+++ server/Application/Venues/Queries/GetVenuesOutputModel.cs
@@ -0,0 +1,4 @@
+﻿namespace Application.Venues.Queries
+{
+    public record GetVenuesOutputModel(Guid Id, string Name);
+}
---
+++ server/Application/Venues/Queries/GetVenuesQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Venues.Queries
+{
+    using ErrorOr;
+    using MediatR;
+
+    public record GetVenuesQuery : IRequest<ErrorOr<IEnumerable<GetVenuesOutputModel>>>;
+}
