0599d54b9c92ac4410c64bf00c6f91b4e87a750f
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-05-15 11:40:34 +0300

    add: eventual consistency middleware

---
+++ server/Infrastructure/Middleware/EventualConsistencyMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Common;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Middleware
+{
+    public class EventualConsistencyMiddleware
+    {
+        public const string DomainEventsKey = "DomainEventsKey";
+
+        private readonly RequestDelegate _next;
+
+        public EventualConsistencyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IPublisher publisher, ApplicationDbContext dbContext)
+        {
+            var transaction = await dbContext.Database.BeginTransactionAsync();
+            context.Response.OnCompleted(async () =>
+            {
+                try
+                {
+                    if (context.Items.TryGetValue(DomainEventsKey, out var value) && value is Queue<IDomainEvent> domainEvents)
+                    {
+                        while (domainEvents.TryDequeue(out var nextEvent))
+                        {
+                            await publisher.Publish(nextEvent);
+                        }
+                    }
+
+                    await transaction.CommitAsync();
+                }
+                catch (Exception e)
+                {
+                    // Handle eventual consistency exceptions
+                }
+                finally
+                {
+                    await transaction.DisposeAsync();
+                }
+            });
+
+            await _next(context);
+        }
+    }
+}
